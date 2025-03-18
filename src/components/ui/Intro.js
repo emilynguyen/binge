d3bce041b7517224ac98976d3b47dadc64b768f7
Emily Nguyen <emilymylinh@gmail.com>
2025-03-17 17:52:55 -0700

    Clean up

--- src/components/ui/Intro.js
+++ src/components/ui/Intro.js
@@ -22,30 +22,30 @@ const Intro = ({ handleCloseIntro=null, className="" }) => {
             <Image src={credit} width={89} height={77} style={{ width: '5.5625rem' }} alt="Made with <3 by Emily"/>
           </a>
           {/* Illustrations */}
-          <div className="absolute z-0 pointer-events-none h-screen w-screen lg:h-full lg:w-full">
+          <div className="absolute z-0 pointer-events-none  w-full h-full">
             <Image 
-              className="absolute mt-[-16rem] ml-[-1rem] mt-[1rem] sm:mt-[2rem] w-[10rem] sm:w-[14.125rem]" 
+              className="absolute top-0 ml-[-1rem] mt-[1rem] sm:mt-[2rem] w-[10rem] sm:w-[14.125rem]" 
               src={sticker1} 
               width={226} 
               height={188} 
               alt=""
             />
             <Image 
-              className="absolute right-0 mr-[-1rem] mt-[-10rem] mt-[4rem] w-[6.1rem] sm:w-[8.625rem]" 
+              className="absolute top-0 right-0 mr-[-1rem] mt-[4rem] w-[6.1rem] sm:w-[8.625rem]" 
               src={sticker2} 
               width={138} 
               height={129} 
               alt=""
             />
             <Image 
-              className="absolute bottom-0 ml-[-1rem] mb-[-10rem] mb-[7rem] w-[5.7rem] sm:w-[8.0625rem]" 
+              className="absolute bottom-0 ml-[-1rem] mb-[7rem] w-[5.7rem] sm:w-[8.0625rem]" 
               src={sticker3} 
               width={129} 
               height={127} 
               alt=""
             />
             <Image 
-              className="absolute right-0 bottom-0 mr-[1rem] sm:mr-[3rem] mb-[-16rem] mb-[1rem] sm:mb-[2rem] w-[8rem] sm:w-[11.125rem]" 
+              className="absolute right-0 bottom-0 mr-[1rem] sm:mr-[3rem] mb-[1rem] sm:mb-[2rem] w-[8rem] sm:w-[11.125rem]" 
               src={sticker4} 
               width={178} 
               height={132} 
